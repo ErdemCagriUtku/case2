85504de9d8ca5f8a6b8c7659eeef76df32e705e2
ErdemCagriUtku <ecagri.utku@gmail.com>
2024-11-15 18:45:10 +0100

    added discount columns and cleaned data

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -11,3 +11,18 @@ transactions_df=loaded_data_array[2]
 
 format_data(product_df, customer_df, transactions_df)
 
+transactions_df=transactions_df[transactions_df['LLP_GC_ORIG']!=0]
+
+transactions_df['DISCOUNT_AMOUNT'] = transactions_df['LLP_GC_ORIG'] - transactions_df['REVENUE_GC_ORIG']
+transactions_df['DISCOUNT_RATE'] = transactions_df['DISCOUNT_AMOUNT']/transactions_df['LLP_GC_ORIG']
+
+# TEST print(transactions_df.isna().sum())
+
+print((transactions_df==0).sum())
+print(transactions_df.isna().sum())
+#print(transactions_df.loc[:,'DISCOUNT_RATE'].mean())
+# TEST print(transactions_df['LLP_LC_ORIG'] - (transactions_df['LOCAL_LIST_PRICE'] * transactions_df['QUANTITY']))
+
+
+
+
