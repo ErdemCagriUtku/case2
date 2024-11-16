4ad18a5c7b30bba51dd81a14be3fe1081e064f68
ErdemCagriUtku <ecagri.utku@gmail.com>
2024-11-16 11:21:23 +0100

    crated the model df

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -30,14 +30,29 @@ merged_main_df = pd.merge(transactions_df, product_df, on=['FISCAL_YEAR', 'PRODU
 
 merged_main_df = pd.merge(merged_main_df, customer_df, on=['FISCAL_YEAR', 'CUST_NO'], how='inner')
 
-print(merged_main_df.columns)
-#
-#
-# transactions_df
-# merged_df =
-#
-# print(merged_df)
-#
+print(merged_main_df['BUSINESS_SEGMENT'].isna().sum())
+
+def volume_weighted_average(values, volumes):
+    return (values * volumes).sum() / volumes.sum()
+
+# Calculate weighted average for each category
+# model_df = merged_main_df.groupby(['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_NO', 'PRODUCT_CODE']).apply(
+#     lambda x: volume_weighted_average(x['DISCOUNT_RATE'], x['LLP_GC_ORIG'])
+# )
+
+model_df = merged_main_df.groupby(['LPG_CODE',  'FISCAL_YEAR', 'CUST_NO','CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE']).agg(
+    volume_weighted_average=('DISCOUNT_RATE', lambda x: volume_weighted_average(x,merged_main_df.loc[x.index,'LLP_GC_ORIG'])),
+    sum_QTY = ('QUANTITY', lambda x: merged_main_df.loc[x.index, 'QUANTITY'].sum()),
+    sum_REVENUE_LC_ORIG = ('REVENUE_LC_ORIG', lambda x: merged_main_df.loc[x.index, 'REVENUE_LC_ORIG'].sum()),
+    sum_REVENUE_GC_ORIG = ('REVENUE_GC_ORIG', lambda x: merged_main_df.loc[x.index, 'REVENUE_GC_ORIG'].sum()),
+    sum_LLP_LC_ORIG=('LLP_LC_ORIG', lambda x: merged_main_df.loc[x.index, 'LLP_LC_ORIG'].sum()),
+    sum_LLP_GC_ORIG=('LLP_GC_ORIG', lambda x: merged_main_df.loc[x.index, 'LLP_GC_ORIG'].sum())
+).reset_index()
+
+print(model_df.info)
+print(model_df.isna().sum())
+
+#'BUSINESS_SEGMENT'
 
 
-
+# CUST_NO
