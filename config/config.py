c10d2d5be117fa8b9e39e2244b718a72f231ff6b
ErdemCagriUtku <ecagri.utku@gmail.com>
2024-11-16 20:41:09 +0100

    finished models, except some bugs

--- config/config.py
+++ config/config.py
@@ -1,3 +1,6 @@
 PRODUCT_PATH = 'C:/Users/cagri/Downloads/Case Study - Part 2/product_master_anonymised.csv'
 CUSTOMER_PATH = 'C:/Users/cagri/Downloads/Case Study - Part 2/customer_master_anonymised.csv'
 TRANSACTIONS_PATH = 'C:/Users/cagri/Downloads/Case Study - Part 2/transactions_anonymised.csv'
+
+
+SAVE_PREDS_PATH = 'C:/Users/cagri/Downloads/Case Study - Part 2/preds_output.csv'
--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -43,7 +43,7 @@ print(merged_main_df['BUSINESS_SEGMENT'].isna().sum())
 
 print(merged_main_df.info)
 print(merged_main_df.dtypes)
-
+print(merged_main_df['DISCOUNT_RATE'])
 
 def volume_weighted_average(values, volumes):
     if volumes.sum() != 0:
@@ -59,70 +59,110 @@ def volume_weighted_average(values, volumes):
 
 model_df = merged_main_df.groupby(['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE']).agg(
      volume_weighted_average = ('DISCOUNT_RATE', lambda x: volume_weighted_average(x, merged_main_df.loc[x.index, 'LLP_GC_ORIG'])),
+     sum_DISCOUNT= ('DISCOUNT_AMOUNT', 'sum'),
      sum_QTY = ('QUANTITY', 'sum'),
      sum_REVENUE_LC_ORIG = ('REVENUE_LC_ORIG', lambda x: merged_main_df.loc[x.index, 'REVENUE_LC_ORIG'].sum()),
      sum_REVENUE_GC_ORIG = ('REVENUE_GC_ORIG', lambda x: merged_main_df.loc[x.index, 'REVENUE_GC_ORIG'].sum()),
      sum_LLP_LC_ORIG=('LLP_LC_ORIG', lambda x: merged_main_df.loc[x.index, 'LLP_LC_ORIG'].sum()),
      sum_LLP_GC_ORIG=('LLP_GC_ORIG', lambda x: merged_main_df.loc[x.index, 'LLP_GC_ORIG'].sum())
  ).reset_index()
+#'PRODUCT_TYPE', ->filtered out
 
-# model_df=model_df[model_df['sum_LLP_GC_ORIG']!=0]
-# print(model_df.info)
-# print(model_df.isna().sum())
+
+model_df=model_df[model_df['sum_LLP_GC_ORIG']!=0]
+#model_df['volume_weighted_average']=model_df['sum_DISCOUNT']/model_df['sum_LLP_GC_ORIG']
+
+print (model_df['volume_weighted_average'])
+
+
+model_df=model_df[model_df['sum_LLP_GC_ORIG']!=0]
+print(model_df.info)
+print(model_df.isna().sum())
+print(model_df.dtypes)
+
+# print(merged_main_df.dtypes)
 # print(model_df.dtypes)
-#
-# # print(merged_main_df.dtypes)
-# # print(model_df.dtypes)
-# # print(product_df.dtypes)
-# cols =['BUSINESS_SEGMENT','LPG_CODE',  'FISCAL_YEAR', 'CUST_NO','CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE']
-#
-# model_df[cols] = model_df[cols].astype('category')
-#
-#
-# # Split the data
-# model_df_train=model_df[model_df['FISCAL_YEAR']!=2024]
-# model_df_test=model_df[model_df['FISCAL_YEAR']==2024]
-#
-# model_features=['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE', 'sum_QTY','sum_REVENUE_LC_ORIG', 'sum_REVENUE_GC_ORIG', 'sum_LLP_LC_ORIG', 'sum_LLP_GC_ORIG']
-#
-# X_train=model_df_train[model_features]
-# y_train=model_df_train['volume_weighted_average']
-#
-#
-# X_test=model_df_test[model_features]
-# y_test=model_df_test['volume_weighted_average']
-#
-#
-# # X = model_df[['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE', 'sum_QTY','sum_REVENUE_LC_ORIG', 'sum_REVENUE_GC_ORIG', 'sum_LLP_LC_ORIG', 'sum_LLP_GC_ORIG']]  # Input features
-# # y = model_df['volume_weighted_average']  # Target variable
-#
-# #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
-#
-#
-# # Initialize and train model
-# linear_model = LinearRegression()
-# linear_model.fit(X_train, y_train)
-#
-# # Predict and evaluate
-# y_pred = linear_model.predict(X_test)
-# mse = mean_squared_error(y_test, y_pred)
-# print(f"Mean Squared Error: {mse}")
-#
-#
-# ridge_model = Ridge(alpha=1.0)
-# ridge_model.fit(X_train, y_train)
-# ridge_pred = ridge_model.predict(X_test)
-# print(f"Ridge Regression MSE: {mean_squared_error(y_test, ridge_pred)}")
-#
-#
-#
-# lasso_model = Lasso(alpha=0.1)
-# lasso_model.fit(X_train, y_train)
-# lasso_pred = lasso_model.predict(X_test)
-# print(f"Lasso Regression MSE: {mean_squared_error(y_test, lasso_pred)}")
+# print(product_df.dtypes)
+cols =['BUSINESS_SEGMENT','LPG_CODE',  'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE']
 
-model_df['count_rows']=model_df.groupby(['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_NAME','CUST_ADRESSE_ORT'])['FISCAL_YEAR'].transform('count')
-print(model_df[model_df['count_rows']!=1])
+model_df[cols] = model_df[cols].astype('category')
+
+
+# Split the data
+model_df_train=model_df[model_df['FISCAL_YEAR']!=2024]
+model_df_test=model_df[model_df['FISCAL_YEAR']==2024]
+
+model_features=['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE', 'sum_QTY','sum_REVENUE_LC_ORIG', 'sum_REVENUE_GC_ORIG', 'sum_LLP_LC_ORIG', 'sum_LLP_GC_ORIG']
+#'PRODUCT_TYPE'-> filtered out
+
+X_train=model_df_train[model_features]
+y_train=model_df_train['volume_weighted_average']
+
+
+X_test=model_df_test[model_features]
+y_test=model_df_test['volume_weighted_average']
+
+
+# X = model_df[['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_ADRESSE_ORT','CUST_NAME','CUST_TYPE', 'sum_QTY','sum_REVENUE_LC_ORIG', 'sum_REVENUE_GC_ORIG', 'sum_LLP_LC_ORIG', 'sum_LLP_GC_ORIG']]  # Input features
+# y = model_df['volume_weighted_average']  # Target variable
+
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+
+
+# Initialize and train model
+linear_model = LinearRegression()
+linear_model.fit(X_train, y_train)
+
+# Predict and evaluate
+y_pred = linear_model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+
+#ridge_model = Ridge(alpha=100.0)
+#ridge_model = Ridge(alpha=10.0)
+ridge_model = Ridge(alpha=1.0)
+ridge_model.fit(X_train, y_train)
+ridge_pred = ridge_model.predict(X_test)
+print(f"Ridge Regression MSE: {mean_squared_error(y_test, ridge_pred)}")
+
+
+
+#lasso_model = Lasso(alpha=100.0)
+#lasso_model = Lasso(alpha=10.0)
+lasso_model = Lasso(alpha=0.1)
+lasso_model.fit(X_train, y_train)
+lasso_pred = lasso_model.predict(X_test)
+print(f"Lasso Regression MSE: {mean_squared_error(y_test, lasso_pred)}")
+
+
+#duplicate check
+# model_df['count_rows']=model_df.groupby(['LPG_CODE', 'BUSINESS_SEGMENT', 'FISCAL_YEAR', 'CUST_NAME','CUST_ADRESSE_ORT'])['FISCAL_YEAR'].transform('count')
+# print(model_df[model_df['count_rows']!=1])
+
+pred_df=model_df[model_features]
+pred_df['FISCAL_YEAR']=2025
+pred_df['PREDS_2025']=lasso_model.predict(pred_df)
+
+print(pred_df['PREDS_2025'].mean(axis=0))
+
+print(pred_df)
+print(model_df_train['volume_weighted_average'].mean(axis=0))
+
+pred_df[cols] = pred_df[cols].astype('int64')
+
+final_preds=pred_df.groupby(['LPG_CODE', 'FISCAL_YEAR','CUST_NAME']).agg(
+     aggregated_2025_preds = ('PREDS_2025', lambda x: volume_weighted_average(x, pred_df.loc[x.index, 'sum_LLP_GC_ORIG']))
+ ).reset_index()
+
+print(final_preds)
+
+final_preds.to_csv(config.config.SAVE_PREDS_PATH, index=False)
+
+#
+# print(pred_df['aggregated_2025_preds'].mean(axis=0))
+#
+# print(merged_main_df.iloc[:,0:].mean(axis=0))
 
 
 #ayni df ile 2025 icin predictionlar yapilabilir ve bir müsteri icin birden fayla pred varsa ortalanabilir
